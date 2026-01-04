00a72e00c82d873933e72b1531a85fc2c2390655
ngjupeng <jupeng2015@gmail.com>
2026-01-04 14:31:11 +0800

    test commit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -6,4 +6,17 @@ return {
 	keys = {
 		{ "<leader>gg", "<cmd>LazyGit<CR>", desc = "Open LazyGit" },
 	},
+	config = function()
+		vim.g.lazygit_floating_window_scaling_factor = 0.9
+		vim.g.lazygit_floating_window_use_plenary = 0
+
+		-- Close lazygit with Escape or q when unfocused
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "lazygit",
+			callback = function()
+				vim.keymap.set("n", "<Esc>", "<cmd>close<CR>", { buffer = true, silent = true })
+				vim.keymap.set("n", "q", "<cmd>close<CR>", { buffer = true, silent = true })
+			end,
+		})
+	end,
 }
